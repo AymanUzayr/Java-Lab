285151c47c482d5a54e6b9502e1235f211452736
AymanUzayr <68193240+AymanUzayr@users.noreply.github.com>
2024-07-16 01:38:27 +0530

    Update Generics.java

--- Collections/Generics.java
+++ Collections/Generics.java
@@ -1,6 +1,12 @@
 public class Generics {
-
     public static void main(String[] args) {
-        
-    }
+        List<Fruit> fruits = new ArrayList<>();
+        fruits.add(new Fruit("Apple"));
+        fruits.add(new Fruit("Banana"));
+        fruits.add(new Fruit("Mango"));
+
+        System.out.println("Generic List elements:");
+        for(Fruit fruit : fruits) {
+            System.out.print(fruit + "\t");
+        }
 }
